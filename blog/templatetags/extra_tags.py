009e3b2f2186aed050d68284ba1892a1568d14a2
mofei952 <695741200@qq.com>
2018-09-13 16:47:35 +0800

    template tag no use

--- blog/templatetags/extra_tags.py
+++ blog/templatetags/extra_tags.py
@@ -9,12 +9,13 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag
-def get_display_page_range(num_pages,num, display_count=5):
+def get_display_page_range(num_pages, num, display_count=5):
     p = display_count // 2
     s = max(num - p, 1)
     e = min(num + 2 * p, num_pages)
     if e - s + 1 > display_count:
         e -= e - s + 1 - display_count
-    print(num, s, e)
-    return range(s,e+1)
+    # print(num, s, e)
+    return range(s, e + 1)
